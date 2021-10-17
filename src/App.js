a25a2f51ef6b84412b7f2d64f1035e7bf1235c36
matheus <m.reissilva@protonmail.com>
2021-10-16 21:00:24 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Fragment } from 'react';
+import axios from 'axios';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h3>Clima nas suas Coordenadas (Exemplo)</h3>
+      <hr />
+      <ul>
+        <li>Temperatura atual: x°</li>
+        <li>Temperatura máxima: x°</li>
+        <li>Temperatura minima: x°</li>
+        <li>Pressão: x hpa</li>
+        <li>Umidade: x%</li>
+      </ul>
+    </Fragment>
   );
 }
-
 export default App;
